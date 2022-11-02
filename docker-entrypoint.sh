5561ec6bda075bb26db8ab159fab13acc9e022e9
xbexbex <petja.valkama@gmail.com>
2022-11-02 17:08:45 +0200

    Add jupyter server

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-poetry install
-poetry run uvicorn app.main:app --reload --host 0.0.0.0 --port 80 --reload-include "app/*" --reload-exclude "*/__pycache__"
+if [ "$IS_PRODUCTION" = 1 ]; then
+    poetry install --no-dev
+    poetry run uvicorn app.main:app --host 0.0.0.0 --port 80
+else
+    poetry install
+    poetry run uvicorn app.main:app --reload --host 0.0.0.0 --port 80 --reload-include "app/*" --reload-exclude "*/__pycache__" &
+    poetry run jupyter notebook --ip='*' --NotebookApp.token='asdf' --NotebookApp.password='' --allow-root -i 0.0.0.0 --allow_remote_access=true
+fi
