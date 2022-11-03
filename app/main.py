e17c3d0b371cabdd1790571126e568f26f600115
xbexbex <petja.valkama@gmail.com>
2022-11-03 20:21:00 +0200

    Add basic calculation endpoint

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from app.calculator.calculator import CarbonCalculator
 
 app = FastAPI()
 
+cc = CarbonCalculator()
 
-@app.get("/")
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    return {"filename": file.filename}
+
+
+@app.get("/calc")
 async def root():
-    return {"message": "Hello World"}
+    sum = cc.calculate("data/vantaa_yk.shp")
+    return sum
