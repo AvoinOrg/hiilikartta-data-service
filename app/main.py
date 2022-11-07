7ef643d0be2720182f4d3d45e75c7758043af133
xbexbex <petja.valkama@gmail.com>
2022-11-07 18:31:43 +0000

    Add cors

--- app/main.py
+++ app/main.py
@@ -1,16 +1,26 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.calculator.calculator import CarbonCalculator
 
 app = FastAPI()
 
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 cc = CarbonCalculator()
 
-@app.post("/uploadfile/")
+
+@app.post("/calculate")
 async def create_upload_file(file: UploadFile):
-    return {"filename": file.filename}
-
-
-@app.get("/calc")
-async def root():
-    sum = cc.calculate("data/vantaa_yk.shp")
-    return sum
+    sum, area = cc.calculate(file.file)
+    return {"sum": sum, "area": area}
