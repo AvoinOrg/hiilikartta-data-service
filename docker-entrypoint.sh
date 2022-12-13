1014ec6f7cf747ce0d3578043937b1bd5f13681e
xbexbex <petja.valkama@gmail.com>
2022-12-13 09:37:36 +0000

    Update dockerfiles

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,6 @@ if [ "$IS_PRODUCTION" = 1 ]; then
 else
     poetry install
     autossh -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -nNT -L 5432:localhost:"${REMOTE_DB_PORT}" ${REMOTE_DB_CONNECTION_STRING} -i /root/.ssh/id_rsa &
-    poetry run uvicorn app.main:app --reload --host 0.0.0.0 --port 80 --reload-dir "app" &
+    poetry run uvicorn app.main:app --reload --host 0.0.0.0 --port 80 --reload-exclude "*" --reload-include "app/**/*" &
     poetry run jupyter notebook --ip='*' --NotebookApp.token="${NOTEBOOK_TOKEN}" --NotebookApp.password='' --allow-root
 fi
