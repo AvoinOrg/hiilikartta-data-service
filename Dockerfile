8e50aff0198343aae7021f58e40b6fddb2f95b93
xbexbex <petja.valkama@gmail.com>
2022-12-18 19:26:16 +0000

    Update docker scripts

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,11 @@
 FROM python:3.9.15
 
+SHELL ["/bin/bash", "-l", "-c"]
+
 WORKDIR /app
 
 RUN apt-get update &&\ 
     apt-get install autossh -y &&\
-    pip install poetry
+    pip install poetry &&\
+    sed -i '1,6d' /root/.bashrc &&\
+    echo "poetry shell" >> /root/.bashrc
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+source /root/.bashrc >/dev/null 2>&1
+
 if [ "$IS_PRODUCTION" = 1 ]; then
     poetry install --no-dev
     poetry run uvicorn app.main:app --host 0.0.0.0 --port 80
 else
     poetry install
-    autossh -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -nNT -L 5432:localhost:"${REMOTE_DB_PORT}" ${REMOTE_DB_CONNECTION_STRING} -i /root/.ssh/id_rsa &
+    autossh -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -nNT -L "${PG_PORT}":localhost:"${REMOTE_DB_PORT}" ${REMOTE_DB_CONNECTION_STRING} -i /root/.ssh/remote_db_rsa &
     poetry run uvicorn app.main:app --reload --host 0.0.0.0 --port 80 --reload-exclude "*" --reload-include "app/**/*" &
     poetry run jupyter notebook --ip='*' --NotebookApp.token="${NOTEBOOK_TOKEN}" --NotebookApp.password='' --allow-root
 fi
