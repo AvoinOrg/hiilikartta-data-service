6ab382e8a8ef5d7b4e0d5cff11cecce48a08607d
xbexbex <petja.valkama@gmail.com>
2023-09-05 07:52:18 +0000

    Add function for processing variables query

--- app/calculator/calculator.py
+++ app/calculator/calculator.py
@@ -4,7 +4,11 @@ import geopandas as gpd
 import xarray as xr
 from geocube.api.core import make_geocube
 
-from app.database.query import fetch_raster_for_region
+from app.database.query import (
+    fetch_variables_for_region,
+    fetch_bio_carbon_for_region,
+    fetch_ground_carbon_for_region,
+)
 
 
 ha_to_grid = 16 * 16 / 10000
@@ -37,6 +41,41 @@ class CarbonCalculator:
 
         self.zone_raster = zone_raster
 
+    async def get_variables(self, wkt: str, crs: str) -> xr.Dataset:
+        rows, column_names = await fetch_variables_for_region(self.db_session, wkt, crs)
+
+        datasets = []
+
+        for row in rows:
+            rast = row[column_names.index("tiff")]
+
+            with rio.MemoryFile(rast).open() as dataset:
+                data_array = rxr.open_rasterio(dataset, masked=True)
+
+            # 1. Convert data_array to a dataset
+            ds = data_array.to_dataset(name="raster")
+
+            # 2. Add other variables from the row to the dataset
+            for col_name in column_names:
+                if col_name != "tiff":
+                    # Add as a data variable (with the same value for all coordinates since it's a scalar)
+                    ds[col_name] = ("y", "x"), [
+                        [row[column_names.index(col_name)]] * len(ds["x"])
+                    ] * len(ds["y"])
+
+            # 3. Append the dataset to datasets
+            datasets.append(ds)
+
+        variables_ds = datasets[0]
+
+        for ds in datasets[1:]:
+            variables_ds = xr.combine_by_coords(
+                [variables_ds, ds], combine_attrs="override"
+            )
+
+        variables_ds = variables_ds.squeeze(dim="band", drop=True)
+
+        return variables_ds
     async def calculate(self):
         wkt = self.zone.geometry.unary_union.wkt
 
