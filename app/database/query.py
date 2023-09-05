562ad69519b2fbbd7978bdb5d066b1f80a1e97ab
xbexbex <petja.valkama@gmail.com>
2023-09-05 07:50:35 +0000

    Update raster query to variables query

--- app/database/query.py
+++ app/database/query.py
@@ -7,32 +7,40 @@ from app.utils.logger import get_logger
 logger = get_logger(__name__)
 
 
-async def fetch_raster_for_region(db_session: AsyncSession, wkt, crs):
+async def fetch_variables_for_region(db_session: AsyncSession, wkt: str, crs: str):
+    crs = int(crs)
+
     try:
         statement = text(
             f"""
-                    SELECT
-                    ST_AsTIFF(ST_Union(rast), 'LZW') as tiff
-                    FROM luke_mvmi2015_segm_bmsum10kgha
-                    WHERE ST_Intersects(
-                    rast, 
+                SELECT 
+                    luke_mvmisegls_2021_muuttujat.*,
+                    ST_AsTIFF(rast, 'LZW') as tiff
+                FROM luke_mvmisegls_2021_muuttujat
+                JOIN luke_mvmisegls_2021_id 
+                    ON luke_mvmisegls_2021_muuttujat.kuvio_id = luke_mvmisegls_2021_id.rid
+                WHERE ST_Intersects(
+                    luke_mvmisegls_2021_id.rast,
                     ST_Transform(
                         ST_SetSRID(
-                            ST_GeomFromText(
-                                :wkt
-                            ), 
+                            ST_GeomFromText(:wkt),
                             :crs
-                        ), 
+                        ),
                         3067
                     )
-                    );
+                );
                 """
         )
 
         await db_session.execute(text("SET postgis.gdal_enabled_drivers TO 'GTiff';"))
         result = await db_session.execute(statement, {"wkt": wkt, "crs": crs})
 
-        return result.fetchall()[0][0]
+        rows = result.fetchall()
+
+        # Fetch column names
+        column_names = list(result.keys())
+
+        return rows, column_names
 
     except SQLAlchemyError as ex:
         logger.exception(ex)
