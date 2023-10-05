a0448262506cb4ebe6a9a2a76b3431eefdaa12d2
xbexbex <petja.valkama@gmail.com>
2023-10-05 13:44:32 +0000

    Comment out redundant code

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,6 @@
 source /root/.bashrc >/dev/null 2>&1
 
 poetry install
-autossh -4 -v -M 0 -o "StrictHostKeyChecking no" -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -nNT -L "${PG_PORT}":0.0.0.0:"${REMOTE_DB_PORT}" ${REMOTE_DB_CONNECTION_STRING} -i /root/.ssh/remote_db_rsa &
+# autossh -4 -v -M 0 -o "StrictHostKeyChecking no" -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -nNT -L "${PG_PORT}":0.0.0.0:"${REMOTE_DB_PORT}" ${REMOTE_DB_CONNECTION_STRING} -i /root/.ssh/remote_db_rsa &
 poetry run uvicorn app.main:app --host 0.0.0.0 --port 80 --reload --reload-dir "/app" &
 poetry run jupyter notebook --ip='*' --NotebookApp.token="${NOTEBOOK_TOKEN}" --NotebookApp.password='' --allow-root
