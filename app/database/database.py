93501a90cbfb73bd5e61cd19b52afe32c78cbbb7
xbexbex <petja.valkama@gmail.com>
2023-10-10 10:25:52 +0000

    Add state_db in addition to gis_db

--- app/database/database.py
+++ app/database/database.py
@@ -13,10 +13,18 @@ from app.utils.logger import get_logger
 logger = get_logger(__name__)
 
 global_settings = config.get_settings()
-url = global_settings.pg_url
+gis_url = global_settings.gis_pg_url
+state_url = global_settings.state_pg_url
 
-engine = create_async_engine(
-    url,
+gis_engine = create_async_engine(
+    gis_url,
+    future=True,
+    echo=True,
+    json_serializer=jsonable_encoder,
+)
+
+state_engine = create_async_engine(
+    state_url,
     future=True,
     echo=True,
     json_serializer=jsonable_encoder,
@@ -24,21 +32,48 @@ engine = create_async_engine(
 
 # expire_on_commit=False will prevent attributes from being expired
 # after commit.
-AsyncSessionFactory = sessionmaker(
-    engine, autoflush=False, expire_on_commit=False, class_=AsyncSession
+GisAsyncSessionFactory = sessionmaker(
+    gis_engine, autoflush=False, expire_on_commit=False, class_=AsyncSession
+)
+
+StateAsyncSessionFactory = sessionmaker(
+    state_engine, autoflush=False, expire_on_commit=False, class_=AsyncSession
 )
 
 
-async def get_db() -> AsyncGenerator:
-    async with AsyncSessionFactory() as session:
-        logger.debug(f"ASYNC Pool: {engine.pool.status()}")
+async def get_gis_db() -> AsyncGenerator:
+    async with GisAsyncSessionFactory() as session:
+        logger.debug(f"ASYNC Pool: {gis_engine.pool.status()}")
         yield session
 
 
-async def get_async_db() -> AsyncGenerator:
+async def get_state_db() -> AsyncGenerator:
+    async with StateAsyncSessionFactory() as session:
+        logger.debug(f"ASYNC Pool 2: {state_engine.pool.status()}")
+        yield session
+
+
+async def get_async_gis_db() -> AsyncGenerator:
     try:
-        session: AsyncSession = AsyncSessionFactory()
-        logger.debug(f"ASYNC Pool: {engine.pool.status()}")
+        session: AsyncSession = GisAsyncSessionFactory()
+        logger.debug(f"ASYNC Pool: {gis_engine.pool.status()}")
+        yield session
+    except SQLAlchemyError as sql_ex:
+        await session.rollback()
+        raise sql_ex
+    except HTTPException as http_ex:
+        await session.rollback()
+        raise http_ex
+    else:
+        await session.commit()
+    finally:
+        await session.close()
+
+
+async def get_async_state_db() -> AsyncGenerator:
+    try:
+        session: AsyncSession = StateAsyncSessionFactory()
+        logger.debug(f"ASYNC Pool: {state_engine.pool.status()}")
         yield session
     except SQLAlchemyError as sql_ex:
         await session.rollback()
