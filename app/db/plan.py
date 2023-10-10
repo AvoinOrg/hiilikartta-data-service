576b685a0d1a4880fdc64d327f329640d1cf9b5c
xbexbex <petja.valkama@gmail.com>
2023-10-10 11:19:21 +0000

    Use plan model in plan

--- app/db/plan.py
+++ app/db/plan.py
@@ -1,63 +1,49 @@
-from sqlalchemy import Column, String, DateTime, Enum, JSON, ForeignKey
-from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.ext.declarative import declarative_base
-from datetime import datetime
-import uuid
-import json
-from app.types.general import CalculationStatus
-from app.utils.logger import get_logger
-from datetime import datetime
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import delete
 from sqlalchemy.future import select
-from app.utils.logger import get_logger
+from app.db.models.plan import Plan
+from typing import List, Optional
+from uuid import UUID
 
-logger = get_logger(__name__)
-
-Base = declarative_base()
-
-
-class Plan(Base):
-    __tablename__ = "plan"
-
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    ui_id = Column(String)
-    user_id = Column(String)
-    data = Column(JSON)
-    created_ts = Column(DateTime, default=datetime.utcnow)
-    updated_ts = Column(DateTime, default=datetime.utcnow)
-    calculated_ts = Column(DateTime)
-    calculation_status = Column(
-        Enum(*[status.value for status in CalculationStatus]), nullable=False
-    )
-    report_areas = Column(JSON)
-    report_totals = Column(JSON)
+# Note: I assume the AsyncSession is provided to each function via a parameter (e.g., from FastAPI dependency injection or similar)
 
 
-async def update_plan_status(db_session, calc_id, state, data=None):
-    plan = await db_session.execute(select(Plan).filter_by(id=calc_id))
-    plan = plan.scalar_one_or_none()
+async def get_plan_by_id(db_session: AsyncSession, calc_id: UUID) -> Optional[Plan]:
+    result = await db_session.execute(select(Plan).filter_by(id=calc_id))
+    return result.scalar_one_or_none()
 
+
+async def get_all_plans(db_session: AsyncSession) -> List[Plan]:
+    result = await db_session.execute(select(Plan))
+    return result.scalars().all()
+
+
+async def create_plan(db_session: AsyncSession, plan: Plan) -> Plan:
+    db_session.add(plan)
+    await db_session.commit()
+    await db_session.refresh(plan)
+    return plan
+
+
+async def update_plan_status(
+    db_session: AsyncSession, calc_id: UUID, status: str, data: Optional[dict] = None
+) -> bool:
+    plan = await get_plan_by_id(db_session, calc_id)
     if not plan:
-        logger.error(f"Calculation ID {calc_id} not found in the database!")
         return False
 
-    plan.calculation_status = state
+    plan.calculation_status = status
     if data:
-        plan.data = json.dumps(data)
-
-    if state == CalculationStatus.FINISHED.value:
-        plan.updated_ts = datetime.utcnow()
-        plan.calculated_ts = datetime.utcnow()
-
+        plan.data = data
     await db_session.commit()
     return True
 
 
-async def get_plan_by_id(db_session, calc_id):
-    plan = await db_session.execute(select(Plan).filter_by(id=calc_id))
-    return plan.scalar_one_or_none()
+async def delete_plan(db_session: AsyncSession, calc_id: UUID) -> bool:
+    plan = await get_plan_by_id(db_session, calc_id)
+    if not plan:
+        return False
 
-
-async def create_plan(db_session, id, state):
-    new_plan = Plan(id=id, calculation_status=state)
-    db_session.add(new_plan)
+    await db_session.execute(delete(Plan).filter_by(id=calc_id))
     await db_session.commit()
+    return True
