b01574abaaef014b5a6e72da3fe973867b4e34f4
xbexbex <petja.valkama@gmail.com>
2023-12-20 14:32:33 +0000

    Refactor sql queries

--- app/db/gis.py
+++ app/db/gis.py
@@ -1,105 +1,168 @@
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.sql import text
 from sqlalchemy.ext.asyncio import AsyncSession
+from typing import List
 
 from app.utils.logger import get_logger
 
 logger = get_logger(__name__)
 
 
-async def fetch_variables_for_region(db_session: AsyncSession, wkt: str, crs: str):
-    crs = int(crs)
+async def fetch_variables_for_ids(db_session: AsyncSession, ids: List[str]):
+    try:
+        ids_int = tuple([int(item) for item in ids])
+        statement = text(
+            """
+            SELECT *
+            FROM luke_mvmisegmentit_muuttujat_kokomaa
+            WHERE kuvio = ANY(:ids);
+            """
+        )
+
+        result = await db_session.execute(statement, {"ids": ids_int})
+        col_names = list(result.keys())
+
+        return result.fetchall(), col_names
+
+    except SQLAlchemyError as ex:
+        logger.exception(ex)
+
+
+async def fetch_rasters_for_regions(
+    db_session: AsyncSession, wkts: List[str], crs: str
+):
+    crs_int = int(crs)
 
     try:
+        # Prepare the WKT geometries as a string to use in the SQL query
+        wkt_list_str = ",".join([f"('{wkt}')" for wkt in wkts])
+
         statement = text(
             f"""
+            WITH rasters AS (
                 SELECT 
-                    luke_mvmisegls_2021_muuttujat.*,
-                    ST_AsTIFF(rast, 'LZW') as tiff
-                FROM luke_mvmisegls_2021_muuttujat
-                JOIN luke_mvmisegls_2021_id 
-                    ON luke_mvmisegls_2021_muuttujat.kuvio_id = luke_mvmisegls_2021_id.rid
+                    wkt,
+                    ST_Union(rast) as union_rast,
+                    ST_SetSRID(
+                        ST_GeomFromText(wkt), 
+                        :crs
+                    ) as geom,
+                    idx as order_num
+                FROM luke_mvmisegmentit_id,
+                    unnest(array[{wkt_list_str}]) WITH ORDINALITY as indexed_wkt(wkt, idx)
                 WHERE ST_Intersects(
-                    luke_mvmisegls_2021_id.rast,
-                    ST_Transform(
-                        ST_SetSRID(
-                            ST_GeomFromText(:wkt),
-                            :crs
-                        ),
-                        3067
+                    rast, 
+                    ST_SetSRID(
+                        ST_GeomFromText(wkt), 
+                        :crs
                     )
-                );
-                """
+                )
+                GROUP BY wkt, idx
+            )
+            SELECT 
+                array_agg(ST_AsTIFF(ST_Clip(union_rast, geom), 'DEFLATE9')) as tiffs,
+                order_num
+            FROM rasters
+            GROUP BY wkt, order_num;
+            """
         )
 
-        result = await db_session.execute(statement, {"wkt": wkt, "crs": crs})
+        result = await db_session.execute(statement, {"crs": crs_int})
 
-        rows = await result.fetchall()
+        # Fetching all rows, each row containing a raster for a WKT geometry
+        return result.fetchall()
+    except SQLAlchemyError as ex:
+        logger.exception(ex)
 
-        # Fetch column names
-        column_names = list(result.keys())
 
-        return rows, column_names
+async def fetch_bio_carbon_for_regions(
+    db_session: AsyncSession, wkts: List[str], crs: str
+):
+    crs_int = int(crs)
+
+    try:
+        wkt_list_str = ",".join([f"('{wkt}')" for wkt in wkts])
+
+        statement = text(
+            f"""
+            WITH rasters AS (
+                SELECT 
+                    wkt,
+                    ST_Union(rast) as union_rast,
+                    ST_SetSRID(
+                        ST_GeomFromText(wkt), 
+                        :crs
+                    ) as geom,
+                    idx as order_num
+                FROM hiilikartta_kasvillisuudenhiili_temp_tcha,
+                    unnest(array[{wkt_list_str}]) WITH ORDINALITY as indexed_wkt(wkt, idx)
+                WHERE ST_Intersects(
+                    rast,
+                    ST_SetSRID(
+                        ST_GeomFromText(wkt), 
+                        :crs
+                    )
+                )
+                GROUP BY wkt, idx
+            )
+            SELECT 
+                array_agg(ST_AsTIFF(ST_Clip(union_rast, geom), 'DEFLATE9')) as tiffs,
+                order_num
+            FROM rasters
+            GROUP BY wkt, order_num;
+            """
+        )
+
+        result = await db_session.execute(statement, {"crs": crs_int})
+
+        return result.fetchall()
 
     except SQLAlchemyError as ex:
         logger.exception(ex)
 
 
-async def fetch_bio_carbon_for_region(db_session: AsyncSession, wkt: str, crs: str):
-    crs = int(crs)
+async def fetch_ground_carbon_for_regions(
+    db_session: AsyncSession, wkts: List[str], crs: str
+):
+    crs_int = int(crs)
 
     try:
+        # Prepare the WKT geometries as a string to use in the SQL query
+        wkt_list_str = ",".join([f"('{wkt}')" for wkt in wkts])
+
         statement = text(
             f"""
-                SELECT
-                    ST_AsTIFF(ST_UNION(rast), 'LZW') as tiff
-                    FROM luke_mvmipuustonhiili_2021_tcha
-                    WHERE ST_Intersects(
-                        rast, 
-                            ST_SetSRID(
-                                ST_GeomFromText(
-                                    :wkt
-                                ),
-                            3067
-                        )
-                    );
-                """
+            WITH rasters AS (
+                SELECT 
+                    wkt,
+                    ST_Union(rast) as union_rast,
+                    ST_SetSRID(
+                        ST_GeomFromText(wkt), 
+                        :crs
+                    ) as geom,
+                    idx as order_num
+                FROM gtkjaluke_maaperanhiili_xxxx_tcha,
+                    unnest(array[{wkt_list_str}]) WITH ORDINALITY as indexed_wkt(wkt, idx)
+                WHERE ST_Intersects(
+                    rast, 
+                    ST_SetSRID(
+                        ST_GeomFromText(wkt), 
+                        :crs
+                    )
+                )
+                GROUP BY wkt, idx
+            )
+            SELECT 
+                array_agg(ST_AsTIFF(ST_Clip(union_rast, geom), 'DEFLATE9')) as tiffs,
+                order_num
+            FROM rasters
+            GROUP BY wkt, order_num;
+            """
         )
 
-        result = await db_session.execute(statement, {"wkt": wkt, "crs": crs})
+        result = await db_session.execute(statement, {"crs": crs_int})
 
-        row = result.fetchone()
-        return row[0]
-
-    except SQLAlchemyError as ex:
-        logger.exception(ex)
-
-
-async def fetch_ground_carbon_for_region(db_session: AsyncSession, wkt: str, crs: str):
-    crs = int(crs)
-
-    try:
-        statement = text(
-            f"""
-                SELECT
-                    ST_AsTIFF(ST_UNION(rast), 'LZW') as tiff
-                    FROM gtkjaluke_maaperanhiili_xxxx_tcha
-                    WHERE ST_Intersects(
-                        rast, 
-                            ST_SetSRID(
-                                ST_GeomFromText(
-                                    :wkt
-                                ),
-                            3067
-                        )
-                    );
-                """
-        )
-
-        result = await db_session.execute(statement, {"wkt": wkt, "crs": crs})
-
-        row = result.fetchone()
-        return row[0]
+        return result.fetchall()
 
     except SQLAlchemyError as ex:
         logger.exception(ex)
