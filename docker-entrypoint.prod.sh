6dd275169104d001f283513982d7ef90dd4c0881
xbexbex <petja.valkama@gmail.com>
2024-02-15 16:55:57 +0200

    Add guvicorn

--- docker-entrypoint.prod.sh
+++ docker-entrypoint.prod.sh
@@ -2,4 +2,4 @@
 source /root/.bashrc >/dev/null 2>&1
 
 poetry install --no-dev
-poetry run uvicorn app.main:app --host 0.0.0.0 --port 80
+poetry run gunicorn -k uvicorn.workers.UvicornWorker app.main:app --bind 0.0.0.0:80
