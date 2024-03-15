4b87c475884ee7a716d048a0fc9f16e314e96346
xbexbex <petja.valkama@gmail.com>
2024-03-15 16:16:58 +0000

    Update timestamps

--- app/db/models/plan.py
+++ app/db/models/plan.py
@@ -1,51 +1,73 @@
 from datetime import datetime
-from sqlalchemy import Column, String, DateTime, text, Integer
-from sqlalchemy.dialects.postgresql import UUID, JSONB, ENUM
-from sqlalchemy.orm import declarative_base, Mapped
-from app.types.general import CalculationStatus
+from sqlalchemy import String, DateTime, text, Integer, UniqueConstraint
+from sqlalchemy.dialects.postgresql import UUID, JSONB, ENUM, TIMESTAMP
+from sqlalchemy.sql import func
+from sqlalchemy.orm import Mapped, mapped_column
 from uuid import uuid4
 from typing import Any
 
-Base = declarative_base()
+from app.types.general import CalculationStatus
+from app.db.models.base import Base
 
 
 class Plan(Base):
     __tablename__ = "plan"
+    __table_args__ = (
+        UniqueConstraint(
+            "ui_id", name="ui_id_unique"
+        ),  # Adds a unique constraint to ui_id
+    )
 
-    id: Mapped[UUID] = Column(
+    id: Mapped[UUID] = mapped_column(
         UUID(as_uuid=True),
         primary_key=True,
         default=uuid4,
         server_default=text("gen_random_uuid()"),
     )
-    ui_id: Mapped[str] = Column(UUID)
-    name: Mapped[str] = Column(String)
-    user_id: Mapped[str] = Column(String)
-    data: Mapped[dict[str, Any]] = Column(JSONB)
-    created_ts: Mapped[datetime] = Column(
-        DateTime, default=datetime.utcnow, server_default=text("current_timestamp(0)")
-    )
-    updated_ts: Mapped[datetime] = Column(
-        DateTime,
-        default=datetime.utcnow,
-        server_default=text("current_timestamp(0)"),
-        onupdate=datetime.utcnow,
-    )
-    saved_ts: Mapped[datetime] = Column(
-        DateTime,
-        default=datetime.utcnow,
+    ui_id: Mapped[str] = mapped_column(UUID, index=True)
+    name: Mapped[str] = mapped_column(String, nullable=True)
+    user_id: Mapped[str] = mapped_column(String, nullable=True)
+    data: Mapped[dict[str, Any]] = mapped_column(JSONB, nullable=True)
+    created_ts: Mapped[datetime] = mapped_column(
+        TIMESTAMP,
+        default=func.now,
         server_default=text("current_timestamp(0)"),
     )
-    total_indices: Mapped[int] = Column(Integer)
-    last_index: Mapped[int] = Column(Integer)
-    last_area_calculation_status = Column(
-        ENUM(CalculationStatus, name="calculation_status_enum"), index=True
+    updated_ts: Mapped[datetime] = mapped_column(
+        TIMESTAMP,
+        default=func.now,
+        server_default=text("current_timestamp(0)"),
+        onupdate=func.now,
     )
-    last_area_calculation_retries = Column(Integer)
-    calculated_ts: Mapped[datetime] = Column(DateTime)
-    calculation_updated_ts: Mapped[datetime] = Column(DateTime)
-    calculation_status = Column(
-        ENUM(CalculationStatus, name="calculation_status_enum"), index=True
+    saved_ts: Mapped[datetime] = mapped_column(
+        TIMESTAMP,
+        default=func.now,
+        server_default=text("current_timestamp(0)"),
+        nullable=True,
     )
-    report_areas: Mapped[dict] = Column(JSONB)
-    report_totals: Mapped[dict] = Column(JSONB)
+    # last_accessed_ts: Mapped[datetime] = mapped_column(
+    #     TIMESTAMP,
+    #     default=func.now,
+    #     server_default=text("current_timestamp(0)"),
+    # )
+    total_indices: Mapped[int] = mapped_column(Integer, nullable=True)
+    last_index: Mapped[int] = mapped_column(Integer, nullable=True)
+    last_area_calculation_status = mapped_column(
+        ENUM(CalculationStatus, name="calculation_status_enum"),
+        nullable=True,
+    )
+    last_area_calculation_retries = mapped_column(Integer, default=0, nullable=True)
+    calculated_ts: Mapped[datetime] = mapped_column(DateTime, nullable=True)
+    calculation_updated_ts: Mapped[datetime] = mapped_column(DateTime, nullable=True)
+    calculation_status = mapped_column(
+        ENUM(CalculationStatus, name="calculation_status_enum"),
+        nullable=True,
+    )
+    report_areas: Mapped[dict] = mapped_column(
+        JSONB,
+        nullable=True,
+    )
+    report_totals: Mapped[dict] = mapped_column(
+        JSONB,
+        nullable=True,
+    )
