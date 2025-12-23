7ecb8e1dfa806adae1a0b52735d32c02e9182d65
xbexbex <xbexbex@users.noreply.github.com>
2025-12-23 15:59:38 +0200

    Fix prod worker entrypoint

--- docker-entrypoint-worker.prod.sh
+++ docker-entrypoint-worker.prod.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 source /root/.bashrc >/dev/null 2>&1
-cd /app
-exec poetry run watchmedo auto-restart --directory app --recursive -- \
-    saq app.saq_worker.settings -- --workers 3 --web
+
+poetry run saq --workers 10 app.saq_worker.settings --web
