29d7d38a1914b1821bb26be9257fc57f319e72d1
xbexbex <xbexbex@users.noreply.github.com>
2026-03-01 13:54:01 +0000

    Fix entrypoint

--- docker-entrypoint.prod.sh
+++ docker-entrypoint.prod.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source /root/.bashrc >/dev/null 2>&1
 
-poetry install --no-dev
+poetry install --without dev
 poetry run gunicorn -k uvicorn.workers.UvicornWorker app.main:app --bind 0.0.0.0:80
